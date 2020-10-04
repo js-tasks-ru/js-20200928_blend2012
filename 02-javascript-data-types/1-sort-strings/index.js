4bb69144fca3fc65edf77119b749f7a731aaab0a
Vasilly Kirillov <vasily.kirillov@nordigy.ru>
2020-10-04 16:35:07 +0300

    Add partial solution for sort task

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,6 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+  const newSortedArr = [...arr].sort((a, b) => a.localeCompare(b, 'ru', { caseFirst: 'upper' }));
+  return param === 'asc' ? newSortedArr : newSortedArr.reverse();
 }
